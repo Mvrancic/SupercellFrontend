46bda2d2f5a1c411a656728c271116c56ad8bab5
Mvrancic <mvrancic@mail.austral.edu.ar>
2024-06-02 23:52:20 -0300

    .

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [lecturas, setLecturas] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api')
+      .then(response => {
+        setLecturas(response.data);
+        console.log(response.data);
+        console.log(lecturas);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Lecturas RFID</h1>
+      <ul>
+        {lecturas.map(lectura => (
+          <li key={lectura._id}>{lectura.tarjeta_id}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
